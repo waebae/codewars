0bc4f55b9976a91b638c75ae2491584e64c9a62e
Jeff Wang <jrwuoft@gmail.com>
2023-08-29 16:34:36 -0400

    removed 7kyu files and moved them into 7kyu folder

--- 8kyu/maximum-multiple.js
+++
@@ -1,7 +0,0 @@
-function maxMultiple(divisor, bound){
-  n=bound
-  while (n%divisor!==0){
-    n--
-  }
-  return n
-}
--- 8kyu/simple-fun-2:-circle-of-numbers.js
+++
@@ -1,8 +0,0 @@
-function circleOfNumbers(n, firstNumber){
-  if (firstNumber > (n-1)/2){
-    return firstNumber - n/2
-  }
-  else if (firstNumber < (n-1)/2){
-    return firstNumber + n/2
-  }
-}
