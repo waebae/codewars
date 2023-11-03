82ca8f727682b8fa2abad31dab8b7c136780260a
WaeBae <jrwuoft@gmail.com>
2023-11-02 20:50:02 -0400

    added cycle-a-list-of-values.js

--- 6kyu/cycle-a-list-of-values.js
+++ 6kyu/cycle-a-list-of-values.js
@@ -1,11 +1,21 @@
-function cycle(dir, arr, cur) {
-    let indexValue=0
-    let endingIndex=arr.indexOf(cur)+dir
-    if(arr.indexOf(cur)==-1){
-        console.log(null)
+function pattern(n){
+    newArr=n.toString().split('').map(element=>Number(element))
+    finalArr=[]
+    lastDigit=newArr.pop()
+    let patternStartValue=''
+    let output=[]
+      if(n<1){
+        return ""
     }
-    else{
-        let index=((((dir+arr.indexOf(cur))%arr.length + arr.length)%arr.length))
-        console.log(arr[index])
+    for (let i=0;i<n;i++){
+        newArr[i]=lastDigit
     }
+for(let j=0;j<n;j++){
+    for (let i=j;i<n;i++){
+        newArr[i]=lastDigit
+    }
+    finalArr.push(newArr.join(''))
+    lastDigit=(lastDigit-1<0) ? 9:lastDigit-1
+    }
+return(finalArr.join('\n'))
 }
