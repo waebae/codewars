2b8b8086919605050eee1263f36e5a2db25f3af3
WaeBae <jrwuoft@gmail.com>
2024-01-06 16:08:21 -0500

    completed last-digit-of-a-large-number.js

--- 5kyu/last-digit-of-a-large-number.js
+++ 5kyu/last-digit-of-a-large-number.js
@@ -5,12 +5,53 @@
 //I learned so today Jan 5th 2023 that even BigInt is pretty limited on integer size
 
 function lastDigit(n, m) { 
-    exp = ((m%4n) == 0n) ? 4n :
-    m%4n;
-
-    res = Math.pow(n[Math.floor(n/10n)], exp);
-    console.log(res)
+    if(typeof(n)!==BigInt){
+        n=BigInt(n)
+    }
+    if(typeof(m)!==BigInt){
+        m=BigInt(m)
+    }
+    let modulusM=m%4n
+    let patternArray=[]
+    if(m==0n){
+        return(1n)
+    }
+    else if(n%10n==1n){
+        return(1n)
+    }
+    else if(n%10n==0n){
+        return(0n)
+    }
+    else if(n%10n==5n){
+        return(5n)
+    }
+    else if(n%10n==6n){
+        return(6n)
+    }
+    else if(n%10n==2n){ //if last digit of base number is 2
+        patternArray=[6,2,4,8]
+        return (BigInt(patternArray[modulusM]))
+    }
+    else if(n%10n==3n){
+        patternArray=[1,3,9,7]
+        return (BigInt(patternArray[modulusM]))
+    }
+    else if(n%10n==4n){
+        patternArray=[6,4]
+        return (BigInt(patternArray[modulusM%2n]))
+    }
+    else if(n%10n==7n){
+        patternArray=[1,7,9,3]
+        return (BigInt(patternArray[modulusM]))
+    }
+    else if(n%10n==8n){
+        patternArray=[6,8,4,2]
+        return (BigInt(patternArray[modulusM]))
+    }
+    else if(n%10n==9n){
+        patternArray=[1,9]
+        return (BigInt(patternArray[modulusM%2n]))
+    }
 }
-lastDigit(4n, 1n)
-// lastDigit(10n,10000000000n)
-lastDigit(1606938044258990275541962092341162602522202993782792835301376n, 2037035976334486086268445688409378161051468393665936250636140449354381299763336706183397376n)
+
+lastDigit(31, 73)
