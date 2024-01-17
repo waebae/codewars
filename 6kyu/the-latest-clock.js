51dc85e5451c5d8871f4978516a01cce6e651457
WaeBae <jrwuoft@gmail.com>
2024-01-16 20:28:55 -0500

    the-latest-clock.js saving block before restarting

--- 6kyu/the-latest-clock.js
+++ 6kyu/the-latest-clock.js
@@ -2,9 +2,43 @@ function latestClock(a, b, c, d) {
     /* */
     let array = []
     array.push(a,b,c,d)
-    array.splice(2,0,":")
-    console.log(array.join(""))
+    array.sort((a,b)=>b-a)
+    console.log(array)
+    
+    //First find largest number between the elements that can concat in <=23
+    function firstHalf(array){
+       
+        return array
 
-    return array.join("")
+    }
+    firstHalf(array)
+    //Find the second half
+    let result = ""
+    let secondHalfStart = firstHalf.split('')
+    let filteredArray=JSON.parse(JSON.stringify(array))
+    for(index in secondHalfStart){
+        secondHalfStart[index]=+secondHalfStart[index]
+    }
+    console.log(secondHalfStart)
+    let newArr=filteredArray.filter(val => !secondHalfStart.includes(val));
+    console.log(newArr)
+    let secondHalf=[]
+    secondHalf.push(newArr[0].toString()+newArr[1])
+    secondHalf.push(newArr[1].toString()+newArr[0])
+    secondHalf.sort((a,b)=>b-a)
+    for (index in secondHalf){
+        if(secondHalf[index]<=59){
+            result = firstHalf + secondHalf[index]
+        }
+    }
+
+    
+    console.log(result)
+    console.log(typeof(result))
+    const output=result.split('')
+    output.splice(2,0,":")
+    console.log(output.join(""))
+
+    return output.join("")
 }
-latestClock(1, 9, 8, 3)
+latestClock(9,1,2,5)
