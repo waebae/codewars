fa69b13da9ca60708cb0d1ee395f0be891048746
WaeBae <jrwuoft@gmail.com>
2024-01-16 21:08:55 -0500

    the-latest-clock.js need to re-evaluate program in the event that secondHalf all combinations are greater the 59

--- 6kyu/the-latest-clock.js
+++ 6kyu/the-latest-clock.js
@@ -6,39 +6,47 @@ function latestClock(a, b, c, d) {
     console.log(array)
     
     //First find largest number between the elements that can concat in <=23
-    function firstHalf(array){
-       
-        return array
-
-    }
-    firstHalf(array)
-    //Find the second half
-    let result = ""
-    let secondHalfStart = firstHalf.split('')
-    let filteredArray=JSON.parse(JSON.stringify(array))
-    for(index in secondHalfStart){
-        secondHalfStart[index]=+secondHalfStart[index]
-    }
-    console.log(secondHalfStart)
-    let newArr=filteredArray.filter(val => !secondHalfStart.includes(val));
-    console.log(newArr)
-    let secondHalf=[]
-    secondHalf.push(newArr[0].toString()+newArr[1])
-    secondHalf.push(newArr[1].toString()+newArr[0])
-    secondHalf.sort((a,b)=>b-a)
-    for (index in secondHalf){
-        if(secondHalf[index]<=59){
-            result = firstHalf + secondHalf[index]
+    function firstHalf(arr){
+        let combinations=[]
+        for(index in arr){
+            let currentValue=arr[index]
+            let tempArr=arr.filter(value=>value!=currentValue) //array without current value
+            for (index in tempArr){
+                if(currentValue.toString()+tempArr[index]<=23){
+                    combinations.push(currentValue.toString()+tempArr[index])
+                }
+            }  
         }
+        return combinations
     }
+    result=firstHalf(array)
+    function secondHalf(arr){
+        let value= firstHalf(array).sort((a,b)=>b-a) //not sure if this really sorts highest to lowest or just coincidence
+        highestValueIsolated=value.shift()
+        console.log(highestValueIsolated)
+        highestValueArray=highestValueIsolated.split('')
+        for(index in highestValueArray){
+            highestValueArray[index]=+highestValueArray[index]
+        }
+        let newArr=[...array]
+        lastArr=newArr.filter(x=>!highestValueArray.includes(x))
+        console.log(lastArr)
+        let combinations=[]
+        combinations.push(lastArr[0].toString()+lastArr[1])
+        combinations.push(lastArr[1].toString()+lastArr[0])
+        combinations.sort((a,b)=>(b,a))
+        let largestValue=0
+        for (index in combinations){
+            if(combinations[index]<=59){
+                largestValue=(combinations[index])
+                break
+            }
+        }
 
-    
-    console.log(result)
-    console.log(typeof(result))
-    const output=result.split('')
-    output.splice(2,0,":")
-    console.log(output.join(""))
+        return highestValueIsolated + ":"+largestValue
+    }
+    console.log(secondHalf(result))
 
-    return output.join("")
+
 }
-latestClock(9,1,2,5)
+latestClock(1, 2, 8, 9)
