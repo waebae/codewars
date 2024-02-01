8017ceca1d402260ad7c755ce0487b0def8a0506
WaeBae <jrwuoft@gmail.com>
2024-02-01 16:31:25 -0500

    revised count-the-divisble-numbers.js to O(n), but algo requires O (log n)

--- 6kyu/count-the-divisible-numbers.js
+++ 6kyu/count-the-divisible-numbers.js
@@ -1,13 +1,35 @@
 /* First solution, O(n^2) 
 Too slow, couldn't finish, but works*/
 function divisibleCount(x, y, k) {
-    let allDivisibleNumbers = []
-
+    let allNumbers=[]
+    let count = 0
+    let occurences =0 
+    
     for(i=x;i<=y;i++){
-        if(i%k==0){
-            allDivisibleNumbers.push(i)
-        }
+        allNumbers.push(i)
+        // allNumbers.push({ key : count , value: i})
+        // count++
     }
-    return (allDivisibleNumbers.length)
+    for(i=0;i<allNumbers.length;i++){
+        allNumbers[i]=allNumbers[i]%k
+        // allNumbers[i].value=allNumbers[i].value%k
+    }
+    const result = allNumbers.filter((element) => element < 1);
+
+    // const map = new Map(allNumbers.map((obj) => [obj.key, obj.value]));
+    
+    // map.forEach(value=>{console.log(value)})
+    
+
+    for(i=0;i<result.length;i++){
+        occurences++
+    }
+
+    return(occurences)
+
+    // console.log(map)
+
+    // return (allDivisibleNumbers.length)
  }
  
+ divisibleCount(6,11,2)
