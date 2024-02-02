939e91f234c0017b5f581dbd91312d1683afdf7d
WaeBae <jrwuoft@gmail.com>
2024-02-02 09:17:56 -0500

    added new plan to comments in spelling-bee.js and implemented layout

--- 6kyu/spelling-bee.js
+++ 6kyu/spelling-bee.js
@@ -1,21 +1,53 @@
-/*After seeing how confusing this problem was, my first thought was to just do a
-test for every combination - sort of like tic tac toe. However, I counted maybe
-50-70 possible combinations since each space can have between 2-4 combinations.
-So instead, I am attempting to make one test function for bee and add a count
-every time it returns true*/
+/*Instead of testing each and every space, it would be faster to find all the B
+letters and see how many "bee"s they can spell.*/
 
 
 howManyBees = function(hive) {
-    console.log('test')
-    console.log(hive)
+    let combinedHive=hive[0].concat(hive[1],hive[2])
+    let beeCount = 0
+    console.log(combinedHive)
 
-    function spellsBee(index){
-        if(index==3||index==10||index==17){
+    function findLetterB(input){
+        output=[]
+        for(let i=0;i<combinedHive.length;i++){
+            if(combinedHive[i]=='b'){
+                output.push(i)
+            }
+        }
+        console.log(output)
+        return output
+    }
+
+    const indexOfB = findLetterB(combinedHive)
+    function topCheck(index){
+        if (index<14){ //if index is not on the bottom row, return false
             return false
         }
+    }
+    function bottomCheck(){
+        if (index>7){
+            return false //if index is not on the top row, return false
+        }
+    }
+    function leftCheck(array){ //!! Connect the function call parameter to the statements
+        if (index==2||index==9||index==16||index==6||index==13||index==20){
+            if(combinedHive[index]-1=='e'&&combinedHive[index]-2=='e'){
+                console.log('hi')
+                return true
+            }
+        }
+        else return false
+    }
+    leftCheck(indexOfB)
+    function rightCheck(){
+
+    }
+    function spellsBee(index){
         
     }
+
     
+    spellsBee(indexOfB)
     // for(let )
     // return 0
 }
