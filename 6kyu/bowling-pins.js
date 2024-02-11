70d80e47fda6633d1a1ef96c900e21a1ce53c796
WaeBae <jrwuoft@gmail.com>
2024-02-10 16:08:34 -0800

    edited bowling-pins.js, \n in outputs are in the correct positions, but spacing in between I's is not working properly

--- 6kyu/bowling-pins.js
+++ 6kyu/bowling-pins.js
@@ -1,25 +1,36 @@
 function bowlingPins(arr) {
     let pinArray=[10,9,8,7,6,5,4,3,2,1]
-    console.log(pinArray.indexOf(1))
+
     //remove the index of the elements in arr from pinArray
 
     for ( let i = 0 ; i < arr.length ; i ++ ){
         let pinIndex=pinArray.indexOf(arr[i])
         pinArray.splice(pinIndex,1,0) //we replace all shared values with 0
     }
+    console.log(pinArray)
+
+    for ( let i = 0 ; i < pinArray.length ; i ++ ){ //replaces all non-empty values with I
+
+        pinArray[i]? pinArray.splice(i,1,'I ') :pinArray.splice(i,1,' ')
 
 
-
-    for ( let i = 0 ; i < pinArray.length ; i ++ ){
-        console.log(pinArray[i])
-        pinArray[i] ? pinArray.splice(i,1,'I') :pinArray.splice(i,1,' ')
-        console.log(pinArray[i])
     }
     console.log(pinArray)
-
-    pinArray=pinArray.join(' ')
+    for (let i=0;i<pinArray.length;i++){
+        if(i==4||i==8||i==11){
+            pinArray.splice(i,0,'\n')
+        }
+    }
+    pinArray.splice(3,1,'I')
+    
     
     console.log(pinArray)
+    
+    pinArray=pinArray.join('')
+
+    
+    console.log(pinArray)
+    return(pinArray)
     // let output = []
     // function addBreaks(array){
     //     let output=array
@@ -40,5 +51,8 @@ function bowlingPins(arr) {
 
     // console.log(pinArray[pinArray.length-2])
 }
-bowlingPins([1,2])
+bowlingPins([2, 3, 4])
+
+//'I I I I\n I I I \n       \n   I   '
+//'I I I I\nI I I \n    \n I '
 
