5df2d3a6bd3e02715e08a774e9a0d0d3a260361e
WaeBae <jrwuoft@gmail.com>
2024-02-10 16:18:14 -0800

    bowling-pins.js new pseudo-code plan added

--- 6kyu/bowling-pins.js
+++ 6kyu/bowling-pins.js
@@ -1,4 +1,8 @@
+//I think I should consider making four seperate arrays with lengths of 7 each,
+//and splicing data into those arrays instead of my approach right now. 
+
 function bowlingPins(arr) {
+ 
     let pinArray=[10,9,8,7,6,5,4,3,2,1]
 
     //remove the index of the elements in arr from pinArray
@@ -11,7 +15,16 @@ function bowlingPins(arr) {
 
     for ( let i = 0 ; i < pinArray.length ; i ++ ){ //replaces all non-empty values with I
 
-        pinArray[i]? pinArray.splice(i,1,'I ') :pinArray.splice(i,1,' ')
+        if(pinArray[i] && i == 4){
+            pinArray.splice(i,1,' I ')
+        }
+        else if(pinArray[i] && i == 3){
+            pinArray.splice(i,1,'I')
+        }
+        else if(pinArray[i] == 0 ){
+            pinArray.splice(i,1,' ')
+        }
+        else {pinArray[i]? pinArray.splice(i,1,'I ') :pinArray.splice(i,1,' ')}
 
 
     }
@@ -21,7 +34,7 @@ function bowlingPins(arr) {
             pinArray.splice(i,0,'\n')
         }
     }
-    pinArray.splice(3,1,'I')
+
     
     
     console.log(pinArray)
@@ -31,28 +44,10 @@ function bowlingPins(arr) {
     
     console.log(pinArray)
     return(pinArray)
-    // let output = []
-    // function addBreaks(array){
-    //     let output=array
-    //     for ( let i = 0 ; i < array.length; i++ ){
-    //         if(i==4||i==8||i==11){
-    //             output.splice(i,0,'\n')
-    //         }
-    //     }
-    //     return output
-    // }
-    // let spacedArray = addBreaks(pinArray)
-    // console.log(spacedArray)
 
-    
-    
-    // console.log(spacedArray.toString())
-    
-
-    // console.log(pinArray[pinArray.length-2])
 }
-bowlingPins([2, 3, 4])
+bowlingPins([2, 4])
 
 //'I I I I\n I I I \n       \n   I   '
-//'I I I I\nI I I \n    \n I '
+//'I I I I\n I     \nI I \nI '
 
