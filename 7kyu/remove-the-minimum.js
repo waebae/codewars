15cd3f5cb5d472f5cd8d602c601ff356897ec093
WaeBae <jrwuoft@gmail.com>
2024-02-15 23:59:44 -0500

    7kyu remove-the-minimum.js bugged fixed

--- 7kyu/remove-the-minimum.js
+++ 7kyu/remove-the-minimum.js
@@ -1,18 +1,21 @@
 function removeSmallest(numbers) {
 
-    const array = numbers.slice()
+    const array = [...numbers]
+    const array1 = [...numbers]
     array.sort((a,b)=>a-b)
 
     for ( let i = 0 ; i < numbers.length ; i ++ ){
         if ( numbers[i] == array [0] ){
             let index = numbers.indexOf(numbers[i])
-            numbers.splice((index),1)
+            array1.splice((index),1)
             break
         }
     }
     
     console.log(array)
     console.log(numbers)
-    return numbers
+    return array1
 }   
 removeSmallest([5,3,2,1,4])
+//[ 278, 165, 334, 243, 97, 371, 278, 229, 186, 266, 252, 219, 231, 298, 271 ]
+//[ 278, 165, 334, 243, 97, 371, 278, 229, 186, 266, 252, 27, 219, 231, 298, 271 ]
