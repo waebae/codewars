4d3c0d0808a9ec6ce133979fca11f06d1b7aa5f5
WaeBae <jrwuoft@gmail.com>
2024-02-20 20:05:27 -0500

    7kyu simple-fun-17-rounders.js completed

--- 7kyu/simple-fun-17-rounders.js
+++ 7kyu/simple-fun-17-rounders.js
@@ -1,32 +1,28 @@
 function rounders(value) {
     let valueArray = value.toString().split('')
-    let newValueArray=[...valueArray]
 
-    let array = valueArray.map((number)=>Number(number))
+    const array = [...valueArray]
+    let newArray = [...array].map(element=>Number(element))
 
-    console.log(array)
+    console.log(newArray)
+    for (let i = newArray.length -1 ; i > 0 ; i --){
 
-    for (let i = array.length-1 ; i>-1;i-- ){
-        console.log(typeof(array[i]))
-        if(array[i] >=5 && array[i]!=9 ){ //adding conditional
-            array[i]=0
-            if (array[i]-1){
-                array[i-1]=array[i-1]+1
+        if (newArray[i]>=5){
+            newArray[i]=0
+            if(!array[i-1]){
+                console.log(newArray)
+                newArray.unshift(1)
             }
             else{
-                break
+                newArray[i-1]=newArray[i-1]+1
+                console.log(newArray)
             }
         }
-        else if(array[i]=9 && !array[i-1]){
-            array[i]=0
-            array[i-1]=1
+        else if (newArray[i]<5){
+            newArray[i]=0
         }
-        else if(array[i]<5 && array[i-1]){ //rounding down conditional
-            array[i]=0
-        }
+
     }
-    console.log(array)
-    return Number(array.join(''))
-  
+    console.log(newArray)
+    return Number(newArray.join(''))
 }
-rounders(15)
