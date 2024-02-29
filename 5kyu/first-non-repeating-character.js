93a3c1aa82e2888d5e358820596dcd62f84a9eb2
WaeBae <jrwuoft@gmail.com>
2024-02-29 00:42:25 -0500

    5kyu completed first-non-repeating-character.js

--- 5kyu/first-non-repeating-character.js
+++ 5kyu/first-non-repeating-character.js
@@ -1,6 +1,11 @@
+//After looking at solutions, I was unaware about the string.lastIndexOf()
+//function hahahahah
+
 function firstNonRepeatingLetter(s) {
   let string = s.toLowerCase()
-    
+  if(s == ''){
+    return ('')
+  }
   let uniqueCharacters = (string) => {
     let unique_characters = "";
     for (let i = 0; i < string.length; i++) {
@@ -12,9 +17,17 @@ function firstNonRepeatingLetter(s) {
   };
   let uniqueLetters = (uniqueCharacters(string))
 
-  function findingFirstOccurenceofLetter(string){
-    string=string.split('')
-    
+  for (let i = 0; i < uniqueLetters.length;i++){
+    let index = string.indexOf(uniqueLetters.charAt(i))
+    // console.log(index)
+    if(!(string.includes(uniqueLetters.charAt(i),index+1))){
+      const firstUniqueLetter=uniqueLetters.charAt(i)
+      const index = string.indexOf(firstUniqueLetter)
+      return s.charAt(index)
+    }
+    else if(i==uniqueLetters.length-1){
+      return ('')
+    }
   }
 }
 firstNonRepeatingLetter('sTrseSS')
---
+++ Playground/node_event_loop.js
@@ -0,0 +1,8 @@
+console.log('hi')
+
+setTimeOut (function cb (){
+    console.log('delay')
+
+},5000);
+
+console.log('hi2')
