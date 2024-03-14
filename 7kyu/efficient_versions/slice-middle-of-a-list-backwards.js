523de0597ddf4c5332b7bb72985e40a03a1e085a
WaeBae <jrwuoft@gmail.com>
2024-03-13 22:30:08 -0400

    7kyu added efficient_versions to practice making less time complex code

---
+++ 7kyu/efficient_versions/slice-middle-of-a-list-backwards.js
@@ -0,0 +1,22 @@
+//Turn this code to big O(n)
+function reverseMiddle(array) {
+	newArray=[...array]
+    newArray=[...newArray].reverse()
+    // console.log(newArray)
+    if (newArray.length%2==0){
+        while(newArray.length>2){
+            newArray.pop()
+            newArray.shift()
+        }
+    }
+    else if (newArray.length%2!=0){
+        while(newArray.length>3){
+            newArray.pop()
+            newArray.shift()
+        }
+    }
+    console.log(newArray)
+    
+    return(newArray)
+}
+reverseMiddle([1,2,3,4,5])
