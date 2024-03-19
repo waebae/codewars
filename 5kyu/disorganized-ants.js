978d9015e7d01248ee912b18b1b6cedff1dad046
WaeBae <jrwuoft@gmail.com>
2024-03-19 15:32:52 -0400

    5kyu solved, but too time complex disorganized-ants.js

--- 5kyu/disorganized-ants.js
+++ 5kyu/disorganized-ants.js
@@ -1,32 +1,42 @@
 //Is antsString even necessary when we already have input of ants? Probably not.
 
 function bumpCounter(ants){
-    function leftIndex(index){
-        return index-1
-    }
-    function rightIndex(index){
-        return index+1
-    }
+
     let antsArray = ants.split('')
     let bumpArray = []
     antsArray.forEach((element)=>{
         bumpArray.push(0)
     })
-    console.log(bumpArray)
+    // console.log(bumpArray)
 
     //What I was trying to do here was to join the string back from array so I
     //can quickly test to see if there are any RLs left.
-
-    let antsString=antsArray.join('')
-
-    while(antsString.indexOf('RL')!=-1){
-        antsString=antsString.replace(('RL'),('LR'))
-    }
-    console.log(antsString)
     
-    return(bumpArray)
+    //I can now solve for whata the finished string will look like, but I have
+    //not solved for the amount of collisions
+    let antsString=antsArray.join('')
+    while(antsString.indexOf('RL')!=-1){
+        previousAntsString=antsString
+        antsString=antsString.replace(('RL'),('LR'))
+        //Here I need to write an algorithm that can count the amount of times
+        //collisions occur. My current plan is to find the different letters in
+        //ants and antsString. Then use the index of those letters to increase
+        //bumpArray[index]
+        let antsStringSplit=antsString.split('')
+        currentAntsString=antsString
+
+
+        for (index in previousAntsString){
+            if (previousAntsString[index]!=currentAntsString[index]){
+                bumpArray[index]+=1
+            }
+        }
+    }
+    
+    return(bumpArray.join(' '))
+
 }
 
-bumpCounter('RLRL')
+bumpCounter('RRRLL')
 
 
