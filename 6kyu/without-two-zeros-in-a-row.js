e2a71f3ee2339ef330b957c9b92fdf573af15a09
WaeBae <jrwuoft@gmail.com>
2024-03-31 11:49:57 -0400

    6kyu completed find-all-numbers-without-00.js in progresss: without-two-zeroes-in-a-row.js

---
+++ 6kyu/without-two-zeros-in-a-row.js
@@ -0,0 +1,34 @@
+function zeros(n) {
+
+//Pseudocode
+//Find and iterate through all binary numbers
+//IF n = 0, return 1
+//ELSE startingNumber = '1 + (n amount of zeros behind it)'
+//count = 0
+
+//LOOP
+//WHILE startingNumber.length == n
+    //String(startingNumber).includes('00') ? count ++ ; count = count
+
+//RETURN count
+
+if (n == 1){
+    return 1
+}
+let count = 0
+let startingNumber = 1 
+while (startingNumber.toString().length<n){
+    startingNumber+='0'
+}
+
+while (startingNumber.toString().length <= n){
+    startingNumber.toString().includes('00') ? count=count : count ++
+    startingNumber=Number(startingNumber)
+    console.log(typeof(startingNumber))
+    startingNumber+=1
+}
+console.log(startingNumber)
+console.log(count)
+return(count)
+}
+zeros(3)
